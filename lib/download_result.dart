92b5a73dcfd5633d1cd542fa5bb2eae3d7c033ae
Xanonymous <trusaidlin@gmail.com>
2024-09-12 10:09:04 +0800

    refactor: 💡 eliminate unused json key

--- lib/download_result.dart
+++ lib/download_result.dart
@@ -3,7 +3,7 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'download_result.freezed.dart';
 part 'download_result.g.dart';
 
-@freezed
+@Freezed(toJson: true)
 sealed class DownloadResult with _$DownloadResult {
   const factory DownloadResult({
     @JsonKey(name: 'domain') required String url,
@@ -15,6 +15,6 @@ sealed class DownloadResult with _$DownloadResult {
     @JsonKey(name: 'error_msg') required String errorMsg,
   }) = _DownloadResultError;
 
-  factory DownloadResult.fromJson(Map<String, dynamic> json) =>
-      _$DownloadResultFromJson(json);
+  @override
+  Map<String, dynamic> toJson() => toJson()..remove('runtimeType');
 }
