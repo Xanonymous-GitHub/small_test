9612964b5ba06c968f526394ecfd2c6e5ef91de5
Xanonymous <trusaidlin@gmail.com>
2024-09-14 18:05:19 +0800

    refactor: 💡 rewrite result card for using simple result

--- lib/widgets/download_result_card.dart
+++ lib/widgets/download_result_card.dart
@@ -2,63 +2,54 @@ import 'package:flutter/material.dart';
 import 'package:small_test/download_result.dart';
 
 class DownloadResultCard extends StatelessWidget {
-  final Stream<DownloadResult> _downloadResult;
+  final DownloadResult _downloadResult;
   final String? _timeStamp;
 
   const DownloadResultCard({
     super.key,
-    required Stream<DownloadResult> downloadResult,
+    required DownloadResult downloadResult,
     String? timeStamp,
   })  : _downloadResult = downloadResult,
         _timeStamp = timeStamp;
 
   @override
   Widget build(BuildContext context) {
-    final zeroProgress = RealDownloadResultImplPending(url: Uri.parse(''), progress: 0);
-
-    return StreamBuilder(
-      stream: _downloadResult,
-      initialData: zeroProgress,
-      builder: (context, snapshot) {
-        final data = snapshot.data ?? snapshot.error as RealDownloadResultImplError? ?? zeroProgress;
-        if (data is RealDownloadResultImplPending) {
-          return Card(
-            surfaceTintColor: Theme.of(context).colorScheme.primaryContainer,
-            child: ListTile(
-              title: Text(data.url.toString(), textAlign: TextAlign.center),
-              subtitle: Text(
-                "${(data.progress * 100).toStringAsFixed(2)}%",
-                textAlign: TextAlign.right,
-              ),
-            ),
-          );
-        }
-
-        if (data is RealDownloadResultImplError) {
-          return Card(
-            surfaceTintColor: Theme.of(context).colorScheme.error,
-            child: ListTile(
-              title: Text(data.url.toString(), textAlign: TextAlign.center),
-              subtitle: const Text(
-                'ERROR',
-                textAlign: TextAlign.right,
-              ),
-            ),
-          );
-        }
-
-        final done = data as RealDownloadResultImpl;
-        return Card(
-          surfaceTintColor: Theme.of(context).colorScheme.primaryContainer,
-          child: ListTile(
-            title: Text(done.url.toString(), textAlign: TextAlign.center),
-            subtitle: Text(
-              "${_timeStamp ?? '???'}, ${done.time}ms",
-              textAlign: TextAlign.right,
-            ),
+    if (_downloadResult is RealDownloadResultImplPending) {
+      return Card(
+        surfaceTintColor: Theme.of(context).colorScheme.primaryContainer,
+        child: ListTile(
+          title: Text(_downloadResult.url.toString(), textAlign: TextAlign.center),
+          subtitle: Text(
+            "${(_downloadResult.progress * 100).toStringAsFixed(2)}%",
+            textAlign: TextAlign.right,
           ),
-        );
-      },
+        ),
+      );
+    }
+
+    if (_downloadResult is RealDownloadResultImplError) {
+      return Card(
+        surfaceTintColor: Theme.of(context).colorScheme.error,
+        child: ListTile(
+          title: Text(_downloadResult.url.toString(), textAlign: TextAlign.center),
+          subtitle: const Text(
+            'ERROR',
+            textAlign: TextAlign.right,
+          ),
+        ),
+      );
+    }
+
+    final done = _downloadResult as RealDownloadResultImpl;
+    return Card(
+      surfaceTintColor: Theme.of(context).colorScheme.primaryContainer,
+      child: ListTile(
+        title: Text(done.url.toString(), textAlign: TextAlign.center),
+        subtitle: Text(
+          "${_timeStamp ?? '???'}, ${done.time}ms",
+          textAlign: TextAlign.right,
+        ),
+      ),
     );
   }
 }
