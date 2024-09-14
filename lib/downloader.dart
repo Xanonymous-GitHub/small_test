3cf6a9ed7be2b30e8c52367912fe4c6430186f52
Xanonymous <trusaidlin@gmail.com>
2024-09-14 18:04:03 +0800

    refactor: 💡 rewrite downloader via simple valuenotifier

--- lib/downloader.dart
+++ lib/downloader.dart
@@ -1,41 +1,51 @@
-import 'dart:async';
 import 'dart:collection';
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:small_test/download_result.dart';
 
 /// The [Downloader] class is responsible for benchmarking the time
 /// it takes to download a set of uris.
 /// All the responses are not stored, but expect output their response time.
+/// The [results] field contains the listenable results of the download.
 final class Downloader {
-  final UnmodifiableSetView<Uri> _sourcePaths;
   final http.Client _client;
+  final List<ValueNotifier<DownloadResult>> results;
 
   /// Creates a new [Downloader] instance.
   /// The [sourcePaths] parameter is a set of [Uri]s to download,
-  /// and the [client] parameter is an instance of [http.Client].
-  const Downloader({
+  /// and the [client] parameter is optional, defaulting to a new [http.Client] instance.
+  /// The [results] field is initialized with a list of [RealDownloadResultImplPending] instances.
+  Downloader({
     required UnmodifiableSetView<Uri> sourcePaths,
-    required http.Client client,
-  })  : _sourcePaths = sourcePaths,
-        _client = client;
+    http.Client? client,
+  })  : _client = client ?? http.Client(),
+        results = sourcePaths
+            .map(
+              (uri) => ValueNotifier<DownloadResult>(
+                RealDownloadResultImplPending(url: uri, progress: 0),
+              ),
+            )
+            .toList();
 
-  /// Fetches the target from the given [sourcePath], and discards the response data.
-  /// Different implementations of the [DownloadResult] are returned based on the status of the fetch.
+  /// Fetches the target from the given [pendingResult], and discards the response data.
+  /// Different implementations of the [DownloadResult] are
+  /// returned to [pendingResult] based on the status of the fetch.
   /// The HTTP request is made using the [_client] instance and is always a `GET` request.
   /// Only [HttpStatus.ok] is considered a successful fetch.
-  Stream<DownloadResult> _fetchTarget(Uri sourcePath) async* {
+  void _fetchTarget(int pendingResultIdx) async {
+    final pendingResult = results[pendingResultIdx];
+    final sourcePath = pendingResult.value.url;
     final http.Request request = http.Request('GET', sourcePath);
     final timer = Stopwatch()..start();
 
     try {
-      yield RealDownloadResultImplPending(url: sourcePath, progress: 0);
       final response = await _client.send(request);
 
       if (response.statusCode != HttpStatus.ok) {
         timer.stop();
-        throw RealDownloadResultImplError(
+        pendingResult.value = RealDownloadResultImplError(
           url: sourcePath,
           errorMsg: 'Got non ${HttpStatus.ok} status code from server: $sourcePath',
         );
@@ -44,7 +54,7 @@ final class Downloader {
       final expectContentLength = int.tryParse(response.headers['content-length'] ?? '-1') ?? -1;
       if (expectContentLength == -1) {
         timer.stop();
-        throw RealDownloadResultImplError(
+        pendingResult.value = RealDownloadResultImplError(
           url: sourcePath,
           errorMsg: 'Invalid or missing content-length header from server: $sourcePath',
         );
@@ -54,24 +64,21 @@ final class Downloader {
 
       await for (final chunk in response.stream) {
         byteDownloaded += chunk.length;
-        yield RealDownloadResultImplPending(
+        pendingResult.value = RealDownloadResultImplPending(
           url: sourcePath,
           progress: byteDownloaded / expectContentLength,
         );
       }
 
       timer.stop();
-      yield RealDownloadResultImpl(url: sourcePath, time: timer.elapsedMilliseconds);
+      pendingResult.value = RealDownloadResultImpl(url: sourcePath, time: timer.elapsedMilliseconds);
     } catch (e) {
       timer.stop();
-      throw RealDownloadResultImplError(url: sourcePath, errorMsg: e.toString());
+      pendingResult.value = RealDownloadResultImplError(url: sourcePath, errorMsg: e.toString());
     }
-
-    return;
   }
 
-  /// Fetches all the targets from [_sourcePaths].
-  /// Each of the [_sourcePaths] is fetched concurrently, and the method returns a sequence of [DownloadResult] streams.
-  /// The returned stream indicates the status of each download, and their order is not guaranteed.
-  Iterable<Stream<DownloadResult>> fetchAll() => _sourcePaths.map((path) => _fetchTarget(path).asBroadcastStream());
+  /// Triggers fetches of all the given targets.
+  /// Each of the target is fetched concurrently.
+  void fetchAll() => results.asMap().forEach((idx, _) => _fetchTarget(idx));
 }
