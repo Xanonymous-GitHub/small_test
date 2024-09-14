6b462e48754b8f6a33e73fdf9a2454d5cacbd726
Xanonymous <trusaidlin@gmail.com>
2024-09-14 17:03:30 +0800

    refactor: 💡 rewrite downloader for emitting response progress

--- lib/downloader.dart
+++ lib/downloader.dart
@@ -1,7 +1,7 @@
+import 'dart:async';
 import 'dart:collection';
 import 'dart:io';
 
-import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:small_test/download_result.dart';
 
@@ -21,56 +21,57 @@ final class Downloader {
   })  : _sourcePaths = sourcePaths,
         _client = client;
 
-  /// Fetches the target from the given [sourcePath].
+  /// Fetches the target from the given [sourcePath], and discards the response data.
+  /// Different implementations of the [DownloadResult] are returned based on the status of the fetch.
   /// The HTTP request is made using the [_client] instance and is always a `GET` request.
-  /// Since the response is not stored, the method only returns a boolean indicating whether the fetch was successful.
   /// Only [HttpStatus.ok] is considered a successful fetch.
-  /// The [beforeFetch] and [afterFetch] callbacks are called before and after the fetch respectively.
-  Future<bool> _fetchTargetFrom({
-    required Uri sourcePath,
-    VoidCallback? beforeFetch,
-    VoidCallback? afterFetch,
-  }) {
-    beforeFetch?.call();
-    return _client.get(sourcePath).then((response) {
-      afterFetch?.call();
-      return response.statusCode == HttpStatus.ok;
-    });
+  Stream<DownloadResult> _fetchTarget(Uri sourcePath) async* {
+    final http.Request request = http.Request('GET', sourcePath);
+    final timer = Stopwatch()..start();
+
+    try {
+      yield RealDownloadResultImplPending(url: sourcePath, progress: 0);
+      final response = await _client.send(request);
+
+      if (response.statusCode != HttpStatus.ok) {
+        timer.stop();
+        throw RealDownloadResultImplError(
+          url: sourcePath,
+          errorMsg: 'Got non ${HttpStatus.ok} status code from server: $sourcePath',
+        );
+      }
+
+      final expectContentLength = int.tryParse(response.headers['content-length'] ?? '-1') ?? -1;
+      if (expectContentLength == -1) {
+        timer.stop();
+        throw RealDownloadResultImplError(
+          url: sourcePath,
+          errorMsg: 'Invalid or missing content-length header from server: $sourcePath',
+        );
+      }
+
+      int byteDownloaded = 0;
+
+      await for (final chunk in response.stream) {
+        byteDownloaded += chunk.length;
+        yield RealDownloadResultImplPending(
+          url: sourcePath,
+          progress: byteDownloaded / expectContentLength,
+        );
+      }
+
+      timer.stop();
+      yield RealDownloadResultImpl(url: sourcePath, time: timer.elapsedMilliseconds);
+    } catch (e) {
+      timer.stop();
+      throw RealDownloadResultImplError(url: sourcePath, errorMsg: e.toString());
+    }
+
+    return;
   }
 
   /// Fetches all the targets from [_sourcePaths].
-  /// Each of the [_sourcePaths] is fetched concurrently, and the method returns a sequence of [DownloadResult]s.
-  /// The returned result may have ambiguous order.
-  Future<Iterable<DownloadResult>> _performFetchConcurrently() => Future.wait(
-        _sourcePaths.map(
-          (sourcePath) => compute(
-            (Uri sourcePath) async {
-              final stopwatch = Stopwatch();
-
-              final isSuccess = await _fetchTargetFrom(
-                sourcePath: sourcePath,
-                beforeFetch: stopwatch.start,
-                afterFetch: stopwatch.stop,
-              );
-
-              return isSuccess
-                  ? DownloadResult(
-                      url: sourcePath.toString(),
-                      time: stopwatch.elapsedMilliseconds,
-                    )
-                  : DownloadResult.error(
-                      url: sourcePath.toString(),
-                      errorMsg: 'Failed to fetch the target from $sourcePath',
-                    );
-            },
-            sourcePath,
-          ),
-        ),
-      );
-
-  /// Get all the download results, sorted by the time it took to fetch the target.
-  Future<List<DownloadResult>> fetchAll() async {
-    final results = await _performFetchConcurrently();
-    return results.toList()..sort((a, b) => a.time.compareTo(b.time));
-  }
+  /// Each of the [_sourcePaths] is fetched concurrently, and the method returns a sequence of [DownloadResult] streams.
+  /// The returned stream indicates the status of each download, and their order is not guaranteed.
+  Iterable<Stream<DownloadResult>> fetchAll() => _sourcePaths.map((path) => _fetchTarget(path).asBroadcastStream());
 }
